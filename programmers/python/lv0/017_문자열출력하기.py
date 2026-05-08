16522f89cccbb50cd9e7fddbf1eb336e47964878
youngjin-kim <athw1029@gmail.com>
2026-05-08 23:19:00 +0900

    add: programmers Python Lv0 -a와 b 출력하기

--- programmers/python/lv0/017_문자열출력하기.py
+++ programmers/python/lv0/017_문자열출력하기.py
@@ -1,16 +1,9 @@
 """
 문제: 문자열 출력하기
-링크: https://school.programmers.co.kr/learn/courses/30/lessons/181952?language=python3
+링크: https://school.programmers.co.kr/learn/courses/30/lessons/181952
 
-정수 a와 b를 입력받아 'a = N', 'b = N' 형식으로 두 줄 출력.
-
-입력: 4 5
-출력:
-a = 4
-b = 5
+입력받은 문자열을 그대로 출력.
 """
 
-a, b = map(int, input().strip().split(' '))
-
-print(f'a = {a}')
-print(f'b = {b}')
+str = input()
+print(str)
